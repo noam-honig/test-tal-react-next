be7359178055a2167d1f67c940c3140a09edd8f4
Tal Moskovich <tal.moskovich@gmail.com>
2023-07-06 11:42:43 +0300

    capabilities

--- src/app/api/getAblyToken/route.ts
+++ src/app/api/getAblyToken/route.ts
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
-import ably from "ably/promises"
+import ably from "ably/promises";
 
 export async function GET() {
   const token = await new ably.Rest(
     process.env["ABLY_API_KEY"]!
   ).auth.createTokenRequest({
     capability: {
-      "*": ["subscribe"]
-    }
-  })
-  return NextResponse.json( token )
+      "*": ["subscribe", "publish", "presence"],
+    },
+  });
+  return NextResponse.json(token);
 }
