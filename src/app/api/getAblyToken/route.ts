d6274aa96c45f6dc5818c079a78299442b3873d2
Tal Moskovich <tal.moskovich@gmail.com>
2023-07-16 14:49:49 +0300

    cambly error

--- src/app/api/getAblyToken/route.ts
+++ src/app/api/getAblyToken/route.ts
@@ -2,9 +2,10 @@ import { NextResponse } from "next/server";
 import ably from "ably/promises";
 
 export async function GET() {
-  const token = await new ably.Rest(
-    process.env["ABLY_API_KEY"]!
-  ).auth.createTokenRequest({
+  const token = await new ably.Rest({
+    queryTime: true,
+    key: process.env["ABLY_API_KEY"]!,
+  }).auth.createTokenRequest({
     capability: {
       "*": ["subscribe", "publish", "presence"],
     },
